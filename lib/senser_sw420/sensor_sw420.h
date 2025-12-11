e5ca3341c112f619eaa8b365ef24d4bf210bf786
Loincute <longtakuku@gmail.com>
2025-12-11 16:58:00 +0700

    Delete lib directory

--- lib/senser_sw420/sensor_sw420.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-int readSW420Filtered(int pin, int samples = 10, int delayMs = 2);
-String vibLevelFromValue(int v);
--- lib/sensor_mpu6050/sensor_mpu6050.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <Adafruit_MPU6050.h>
-
-void readMPU6050(Adafruit_MPU6050 &mpu, float &ax, float &ay, float &az, float &gx, float &gy, float &gz);
